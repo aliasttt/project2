e46b102587adcaaf67f1757c3813c3b0d0c8ebdc
aliasttt <aliasadi3853@gmail.com>
2024-02-26 10:17:55 +0300

    strating building login page

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 function hello(){
     alert("this is miguels website")}
+
+
+ 
+
+function ali() {
+    const x = document.querySelector("#submit").value ;
+    alert(`welcome to this website ${x}`)
+    
+
+}
+
+document.querySelector("form").onsubmit = ali()
